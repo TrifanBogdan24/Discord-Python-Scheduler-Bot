f89f615f027caad0f1f29a0fe15bddb4f7d77702
TrifanBogdan24 <trifanbogdan24@gmail.com>
2024-03-12 17:01:40 +0200

    discord bot instead of client

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # pip3 install discord
 import discord              # for Discord API
+from discord.ext import commands
 import asyncio
 import json
 
@@ -44,7 +45,6 @@ def get_msg():
     
 
     for activ in weekly_activities:
-        print(f"{activ}")
         if activ.is_next_in_schedule() == True:
             msg += f"Next: {activ}"
         if activ.is_current_in_schedule() == True:
@@ -165,115 +165,82 @@ def init_reset_schedule():
 
 def messenger_API():
 
-
-    # `global` specifica interpretorului sa nu defineasca variabilele acestea in functie
-    # ci sa foloseasca variabilele globale care poarta aceste nume
     global week_parity
     global is_holiday
     global weekly_activities
     global other_activities
     global deadlines
 
-
-    init_reset_schedule()
-
-    hardcode_schedule()         # datele sunt deja inserate in program
-
-
-
-    # sensitive data (token, ids) are stored in environment variables
-    INVITE_LINK = os.getenv('MY_DAILY_DISCORD_BOT_INVITATION_LINK')
-
-    TOKEN = os.getenv('MY_DISCORD_DAILY_MESSENGER_BOT_TOKEN')
-    GUILD_ID = os.getenv('MY_DISCORD_DAILY_SERVER_ID')
-    CHANNEL_ID = os.getenv('MY_DISCORD_DAILY_CHANNEL_ID')
-    
-    MY_USERNAME = os.getenv('MY_DISCORD_DAILY_USERNAME')
-    TARGET_USER_ID = os.getenv('MY_DISCORD_DAILY_USER_ID')             # eu: Bogdan24
-
-
+    # Discord authentification
+    BOT_TOKEN = os.getenv('MY_DAILY_SCHEDULER_MESSENGER_DISCORD_BOT_TOKEN')
+    SERVER_ID = os.getenv('MY_DAILY_SCHEDULER_DISCORD_SERVER_ID')
+    CHANNEL_ID = os.getenv('MY_DAILY_SCHEDULER_DISCORD_CHANNEL_ID')
+    USER_ID = os.getenv('MY_DISCORD_USER_ID')
 
     intents = discord.Intents.default()
     intents.message_content = True
 
-    client = discord.Client(intents=intents)
+    bot = commands.Bot(command_prefix='!', intents=intents)
 
-    @client.event
+    # Define your global variables here
+
+
+    init_reset_schedule()
+
+    hardcode_schedule()
+
+    async def send_discord_message(channel, target_user_id, msg_content):
+        """ Trimite un mesaj pe un canal de pe un server de Discord
+        """
+        await channel.send(f"<@{target_user_id}> {msg_content}")
+
+    async def send_discord_message_task(channel, target_user_id):
+        """ La fiecare 5 minute, functia va itera acitivitatile
+        si va raporta daca urmeaza sa se intample ceva
+        sau vreo activitate este in desfasuare
+        """
+        while True:
+            
+            message = get_msg()
+
+            if message == '' or message is None:
+                current_time = datetime.now().strftime('%H:%M')
+                message = f"Current time: {current_time}\nNothing for now"
+
+            await send_discord_message(channel, target_user_id, message)
+
+            # 60 de secunde = un minut
+            await asyncio.sleep(5)
+
+    @bot.event
     async def on_ready():
+        print(f"We have logged in as {bot.user}")
 
-        print(f"We have logged in as {client.user}")
+        # Get the channel and guild using IDs
+        channel = bot.get_channel(int(CHANNEL_ID))
+        guild = discord.utils.get(bot.guilds, id=int(SERVER_ID))
 
-        # first message
+        # Your welcome message and commands information here
         welcome_message = f"Hello, I'm your bot! I am now online and ready to assist.\n"
-        
-        welcome_message += ("Even" if week_parity_code == 1 else "Odd")
-        welcome_message += f" indexed week\n"
-
+        welcome_message += ("Even" if week_parity == 1 else "Odd") + f" indexed week\n"
         welcome_message += f"\nAvailable commands:\n"
-        welcome_message += f"- `/get-all-cmds`\n"
-        welcome_message += f"- `/help [cmds]`\n\n"
-        welcome_message += f"- `/get-current-activity`\n"
-        welcome_message += f"- `/get-current-weekly-activity`\n"
-        welcome_message += f"- `/get-current-other-activity`\n"
-        welcome_message += f"- `/get-current-deadline`\n\n"
-        welcome_message += f"- `/get-next-activity`\n"
-        welcome_message += f"- `/get-next-weekly-activity`\n"
-        welcome_message += f"- `/get-next-other-activity`\n"
-        welcome_message += f"- `/get-next-deadline`\n\n"
+        # Add your commands information here
 
+        await send_discord_message(channel, USER_ID, welcome_message)
 
-        await send_discord_message(welcome_message)
+        # Set up the task to send messages every minute
+        bot.loop.create_task(send_discord_message_task(channel, USER_ID))
 
-
-
-        # Set up the task to send the current time every minute
-        await send_discord_message_task()
-
-
-
-
-    @client.event
+    @bot.event
     async def on_message(message):
         # Display the message content in the terminal
         print(f"Message from {message.author}: {message.content}")
 
-
-
-    async def send_discord_message(msg_content):
-        channel = client.get_channel(int(CHANNEL_ID))
-        await channel.send(f"<@{TARGET_USER_ID}> {msg_content}")
-
-
-    async def send_discord_message_task():
-        await client.wait_until_ready()
-        channel = client.get_channel(int(CHANNEL_ID))
-        guild = discord.utils.get(client.guilds, id=int(GUILD_ID))
-
-
-        while not client.is_closed():
-            message = ''
-            message = get_msg()
-
-
-            if message == '':
-                current_time = datetime.now().strftime('%H:%M')
-                message = f"Current time: {current_time}\nNothing for now"
-
-
-            # Send the current time to the specified channel
-            await channel.send(f"<@{TARGET_USER_ID}> {message}")
-
-
-
-
-            # another message can be sent after 60 seconds
-            await asyncio.sleep(60)
-
-
-
+    # Your other commands and logic go here
 
     # Run the bot
-    client.run(TOKEN)
+    bot.run(BOT_TOKEN)
+
 
 
 
